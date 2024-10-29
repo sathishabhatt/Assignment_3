dad5d148eae4f3a0aefeec695281824fad04563b
SathishaBhatt <sathishabhatt@gmail.com>
2024-10-29 14:12:47 +0530

    fix: remove image optimization

--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@
 const nextConfig = {
   basePath: "/Assignment_3",
   output: process.env.NODE_ENV === "development" ? "standalone" : "export",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
